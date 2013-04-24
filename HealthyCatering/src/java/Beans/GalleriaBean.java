d9b29a437af1c3ed5154b44364b59132bc282b62
andreaskalstad <andreaskalstad@trl054101.ra.hist.no>
2013-04-24 14:37:48 +0200

    kdkd

--- HealthyCatering/src/java/Beans/GalleriaBean.java
+++ HealthyCatering/src/java/Beans/GalleriaBean.java
@@ -15,7 +15,6 @@ public class GalleriaBean {
     private String[] descriptions = {null, "Grillspyd av storfe med stekte poteter og salat.", 
                                     "Italiensk spaghetti med kjøttsaus og masse digg.", 
                                     "Masse grønnsaker og biffstrimler dynket i sursøt saus."};
-  
       
     public GalleriaBean(){  
         images = new ArrayList<GalleriaDish>();  
